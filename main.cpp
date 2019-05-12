06130ea7189e94ee9e1a534d5ebd6dc4bf45e735
MROS <yc1043@gmail.com>
2019-05-12 19:46:17 +0800

    暫時拆分測試 sfml 用的程式碼

--- main.cpp
+++ main.cpp
@@ -1,60 +1,7 @@
-#include <cstdio>
-#include <string>
-#include <iostream>
-#include <dirent.h>
-#include <algorithm>
-#include <thread>
-#include <chrono>
-#include <SFML/Graphics.hpp>
-
-using namespace std;
-
-vector<string> dir_list(string dir_name) {
-	vector<string> ret;
-	DIR *dir;
-	struct dirent *ent;
-	if ((dir = opendir(dir_name.c_str())) != nullptr) {
-		while ((ent = readdir(dir)) != nullptr) {
-			ret.emplace_back(ent->d_name);
-		}
-		closedir(dir);
-		sort(ret.begin(), ret.end());
-		return ret;
-	} else {
-		perror("");
-        throw "can't open dir";
-	}
-}
+#include "play_video.h"
 
 int main()
 {
-
-	sf::RenderWindow window(sf::VideoMode(640, 360), "SFML works!");
-
-	auto images = dir_list("../images");
-
-	for (int i = 2; i < images.size() && window.isOpen(); i++) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-		}
-
-		string image = images[i];
-		sf::Texture texture;
-		if (!texture.loadFromFile("../images/" + image)) {
-            cout << "無法開啓檔案: " << image << endl;
-		}
-		sf::Sprite sprite(texture);
-
-		window.clear();
-		window.draw(sprite);
-		window.display();
-
-		this_thread::sleep_for(chrono::milliseconds(1000 / 24));
-	}
-	window.close();
-
+	play_images();
 	return 0;
 }
---
+++ play_video.h
@@ -0,0 +1,11 @@
+#ifndef MPEG_DECODER_PLAY_VIDEO_H
+#define MPEG_DECODER_PLAY_VIDEO_H
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> dir_list(std::string dir_name);
+
+void play_images();
+
+#endif //MPEG_DECODER_PLAY_VIDEO_H
